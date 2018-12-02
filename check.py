443c7be901d58655217c63f47d60fe8fd400372b
rividz <rividz@gmail.com>
2018-12-02 13:33:22 -0800

    Create check.py

---
+++ check.py
@@ -0,0 +1,16 @@
+# Return a list of your container's databases:
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  passwd="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
